473a231fdc4ec4a5818a639c34566b920412f513
MariamDabous <mariam.dabous@hotmail.com>
2022-10-29 23:04:17 +0200

    add exception

--- Lists of Exceptions/Exception.java
+++ Lists of Exceptions/Exception.java
@@ -10,14 +10,16 @@ public class Exception {
         myList.add(48);
         myList.add("Goodbye World");
 
+
+        for(int i = 0; i < myList.size(); i++) {
         try{
-            for(int i = 0; i < myList.size(); i++) {
+            
             Integer castedValue = (Integer) myList.get(i);
             System.out.println(castedValue);
             }
-        }
         catch (java.lang.Exception Rabab){
-            System.out.println("Hello");
+            System.out.println("exceptions at index "+ i + ",the value is "+myList.get(i));
+        }
         }
         
 
